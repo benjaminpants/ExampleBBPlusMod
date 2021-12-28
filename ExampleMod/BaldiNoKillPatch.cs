31febad6b5bd04dd3c565d50c7b8c6f6d26263d0
benjaminpants <maltbyfamily@gmail.com>
2021-12-28 12:12:20 -0800

    episode 2

---
+++ ExampleMod/BaldiNoKillPatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using HarmonyLib;
+
+namespace ExampleMod
+{
+	[HarmonyPatch(typeof(Baldi))]
+	[HarmonyPatch("OnTriggerEnter")]
+	class BaldiNoKillPatch
+	{
+		static bool Prefix()
+		{
+			return false;
+		}
+	}
+}
