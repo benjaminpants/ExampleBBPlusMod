9bffee64f953bdc791d22d3133008a17990a32c6
benjaminpants <maltbyfamily@gmail.com>
2021-12-27 23:38:54 -0800

    Initial commit

---
+++ ExampleMod/BasePlugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+
+namespace ExampleMod
+{
+
+
+	[BepInPlugin("mtm101.rulerp.baldiplus.testmod","Test Baldi Plus Mod","1.0.0.0")]
+	public class BasePlugin : BaseUnityPlugin
+	{
+		void Awake()
+		{
+			Harmony harmony = new Harmony("mtm101.rulerp.baldiplus.testmod");
+			harmony.PatchAll();
+		}
+	}
+}
